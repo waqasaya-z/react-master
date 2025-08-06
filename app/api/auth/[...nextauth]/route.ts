2f785637c1d2e94897f05a25c2197fa765e4a666
Waqas Ayaz <waqasayaz@icloud.com>
2025-08-07 01:34:02 +0500

    Add register api

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,24 +1,60 @@
+import prisma from "@/lib/prisma";
+import { compare } from "bcrypt";
 import NextAuth, { NextAuthOptions } from "next-auth";
+import Credentials from "next-auth/providers/credentials";
 import Github from "next-auth/providers/github";
 import Google from "next-auth/providers/google";
 
 export const authOptions: NextAuthOptions = {
   providers: [
+    Credentials({
+      name: "Credentials",
+      credentials: {
+        email: { label: "Email", type: "text" },
+        password: { label: "Password", type: "password" }
+      },
+      async authorize(credentials) {
+        if (!credentials?.email || !credentials?.password) {
+          throw new Error("Email and password required");
+        }
+        const user = await prisma.user.findUnique({
+          where: { email: credentials.email }
+        });
+
+        if (!user) {
+          throw new Error("User not found");
+        }
+
+        const isValid = await compare(credentials.password, user.password);
+        if (!isValid) {
+          throw new Error("Invalid credentials");
+        }
+
+        return {
+          id: user.id.toString(),
+          email: user.email,
+          name: user.name
+        };
+      }
+    }),
     Google({
       clientId: process.env.GOOGLE_CLIENT_ID as string,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string
     }),
     Github({
       clientId: process.env.GITHUB_CLIENT_ID as string,
-      clientSecret: process.env.GITHUB_CLIENT_SECRET as string,
-    }),
+      clientSecret: process.env.GITHUB_CLIENT_SECRET as string
+    })
   ],
+  session: {
+    strategy: "jwt"
+  },
   secret: process.env.NEXTAUTH_SECRET,
   callbacks: {
     async redirect({ url, baseUrl }) {
       return baseUrl;
-    },
-  },
+    }
+  }
 };
 
 const handler = NextAuth(authOptions);
---
+++ app/api/auth/register/route.ts
@@ -0,0 +1,45 @@
+import prisma from "@/lib/prisma";
+import { hash } from "bcrypt";
+import { NextResponse } from "next/server";
+
+export async function POST(req: Request) {
+  const { name, email, password } = await req.json();
+  if (!email || !password) {
+    return NextResponse.json(
+      { error: "Email and password are required" },
+      { status: 400 }
+    );
+  }
+
+  try {
+    const userExists = await prisma.user.findUnique({ where: { email } });
+    if (userExists)
+      return NextResponse.json(
+        { error: "User already exists" },
+        { status: 400 }
+      );
+
+    const hashedPassport = await hash(password, 10);
+
+    const user = await prisma.user.create({
+      data: {
+        name,
+        email,
+        password: hashedPassport
+      }
+    });
+
+    return NextResponse.json(
+      {
+        message: "User registered successfully",
+        user: { id: user.id, email: user.email }
+      },
+      { status: 201 }
+    );
+  } catch (e) {
+    return NextResponse.json(
+      { error: "Internal server error" },
+      { status: 500 }
+    );
+  }
+}
