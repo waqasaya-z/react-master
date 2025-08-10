3d38f00429155a39bbf39f1bac7d2b4e62c9c7ef
Waqas Ayaz <waqasayaz@icloud.com>
2025-08-10 14:39:20 +0500

    Fix Prisma migrations

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,4 +1,15 @@
 import { PrismaClient } from "./generated/prisma";
 
 const prisma = new PrismaClient();
+
+async function checkConnection() {
+  try {
+    await prisma.$queryRaw`SELECT 1`;
+    console.log('✅ Prisma connected successfully');
+  } catch (error) {
+    console.error('❌ Prisma connection failed:', error);
+  }
+}
+checkConnection();
+
 export default prisma;
---
+++ prisma/migrations/20250810093805_initial_schema/migration.sql
@@ -0,0 +1,20 @@
+-- CreateEnum
+CREATE TYPE "public"."Role" AS ENUM ('USER', 'ADMIN');
+
+-- CreateTable
+CREATE TABLE "public"."User" (
+    "id" SERIAL NOT NULL,
+    "email" TEXT NOT NULL,
+    "name" TEXT,
+    "password" TEXT NOT NULL,
+    "isEmailVerified" BOOLEAN NOT NULL DEFAULT false,
+    "role" "public"."Role" NOT NULL DEFAULT 'USER',
+    "isActive" BOOLEAN NOT NULL DEFAULT true,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "public"."User"("email");
